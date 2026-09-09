650b03a11b0b4547a42e3a3170bf30efb78d12e3
mrmoon01000010 <mrmoon01000010@gmail.com>
2026-09-09 23:31:45 +0000

    Initial setup

---
+++ README.md
@@ -0,0 +1,2 @@
+# Pong Example
+This is a test repository for the game engine EDAME that I am developing. It is simply a clone of pong.
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,43 @@
+#[derive(Clone)]
+#[derive(Copy)]
+struct Movement<const D: usize> {
+    pos: [f32; D],
+    speed: [f32; D],
+}
+impl<const D: usize> Default for Movement<D> {
+    fn default() -> Self {
+        let tmp: [f32; D] = [0.0; D];
+        Self {
+            pos: tmp,
+            speed: tmp,
+        }
+    }
+}
+struct StateSpace {
+    ball: Movement<2>,
+    players: [Movement<1>; 2],
+}
+impl Default for StateSpace {
+    fn default() -> Self {
+        Self {
+            ball: Movement::default(),
+            players: [Movement::default(); 2],
+        }
+    }
+}
+struct ParameterSpace {
+    field_size: [i32; 2],
+    player_size: [i32; 2],
+}
+impl Default for ParameterSpace {
+    fn default() -> Self {
+        Self {
+            field_size: [500, 500],
+            player_size: [25, 100],
+    }
+}
 fn main() {
-    println!("Hello, world!");
+    let p = ParameterSpace::default()
+    let mut s = StateSpace::default();
+    println!("Your speed is {}", s.players[0].speed[0]);
 }
